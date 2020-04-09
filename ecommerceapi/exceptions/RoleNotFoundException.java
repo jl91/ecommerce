8511fac09f4708253854cdf4f52de1f3eccecca8
John Lennon de Melo Fernandes <jlfernandes91@hotmail.com>
2020-04-09 03:32:23 -0300

    removing unused package

--- ecommerceapi/exceptions/RoleNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.profectusweb.ecommerceapi.exceptions;
-
-import java.math.BigInteger;
-
-public class RoleNotFoundException extends Exception {
-    public RoleNotFoundException(BigInteger id) {
-        super(String.format("Role not found for Id: %s", id));
-    }
-}
