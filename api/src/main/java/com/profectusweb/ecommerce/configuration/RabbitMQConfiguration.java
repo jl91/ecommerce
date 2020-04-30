327b7ad6f3b0020cb1f94b14d42e5cca534f99b6
jl91 <plastik69zombieboy@gmail.com>
2020-04-29 23:08:42 -0300

    adding RabbitMQ message sender

---
+++ api/src/main/java/com/profectusweb/ecommerce/configuration/RabbitMQConfiguration.java
@@ -0,0 +1,72 @@
+package com.profectusweb.ecommerce.configuration;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+
+@Configuration
+public class RabbitMQConfiguration {
+    static final String topicExchangeName = "ecommerce-exchange";
+
+    static final String queueName = "save";
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(topicExchangeName);
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue)
+                .to(exchange)
+                .with("");
+    }
+
+    @Bean
+    SimpleMessageListenerContainer rabbitContainer(
+            ConnectionFactory connectionFactory,
+            MessageListenerAdapter listenerAdapter
+    ) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(queueName);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    public MappingJackson2MessageConverter consumerJackson2MessageConverter() {
+        return new MappingJackson2MessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(MessageSaveSubscription receiver) {
+        return new MessageListenerAdapter(receiver, "subscribe");
+    }
+}
